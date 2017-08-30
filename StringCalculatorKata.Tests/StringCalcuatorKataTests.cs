e8e57ea269f01f07ef01c82d3ee07b0243562431
chilli <chilli@chilli.local>
2017-08-30 17:00:35 +0200

    Working on negatives not allowed exception

--- StringCalculatorKata.Tests/StringCalcuatorKataTests.cs
+++ StringCalculatorKata.Tests/StringCalcuatorKataTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using StringCalculatorKata;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,123 @@ using System.Threading.Tasks;
 
 namespace StringCalculatorKata.Tests
 {
-    public class Class1
+    [TestFixture]
+    public class StringCalcuatorKataTests
     {
-    }
+        [Test]
+        public void Add_GivenEmptyString_ShouldReturnZero()
+        {
+            //Arrange
+            var input = "";
+            var expected = 0;
+            var sut = new StringCalculator();
+            //Act
+            var result = sut.Add(input);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenStringOne_ShouldReturnOne()
+        {
+            //Arrange
+            var input = "1";
+            var expected = 1;
+            var sut = new StringCalculator();
+            //Act
+            var result = sut.Add(input);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenStringTwo_ShouldReturnTwo()
+        {
+            //Arrange
+            var input = "2";
+            var expected = 2;
+            var sut = new StringCalculator();
+            //Act
+            var result = sut.Add(input);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenStringThree_ShouldReturnThree()
+        {
+            //Arrange
+            var input = "3";
+            var expected = 3;
+            var sut = new StringCalculator();
+            //Act
+            var result = sut.Add(input);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenStringTwoNumbersSeparetedByComma_ShouldReturnSum()
+        {
+            //Arrange
+            var input = "1,2";
+            var expected = 3;
+            var sut = new StringCalculator();
+            //Act
+            var result = sut.Add(input);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenStringThreeNumbersSeparetedByComma_ShouldReturnSum()
+        {
+            //Arrange
+            var input = "1,2,3";
+            var expected = 6;
+            var sut = new StringCalculator();
+            //Act
+            var result = sut.Add(input);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [Test]
+        public void Add_GivenStringThreeNumbersSeparetedByCommaAndlines_ShouldReturnSum()
+        {
+            //Arrange
+            var input = "1\n2,3";
+            var expected = 6;
+            var sut = new StringCalculator();
+            //Act
+            var result = sut.Add(input);
+            //Assert
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [Test]
+        public void Add_GivenStringChangingDelimeter()
+        {
+            //Arrange
+            var input = "//;\n1;2";
+            var expected = 3;
+            var sut = new StringCalculator();
+            //Act
+            var result = sut.Add(input);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_GivenNegativeNumbersInStringShouldThrowNegativesNotAllowedException()
+        {
+            //Arrange
+            var input = "-2,3";
+            var sut = new StringCalculator();
+            //Assert
+            Assert.Throws<NegativesNotAllowedException>(() => sut.Add(input));
+        }
+    }  
 }
--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StringCalculatorKata;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,75 @@ using System.Threading.Tasks;
 
 namespace StringCalculatorKata
 {
-    public class Class1
+    public class StringCalculator
     {
+        public int Add(string input)
+        {
+            char delimiter = ',';
+            char[] charArray = input.ToCharArray();
+            if(input.StartsWith("//"))
+            {
+                   // delimiter = charArray[2];
+                    input = input.Remove(0, 2);
+                var newdelimiter = input.Substring(0, 1);
+                input = input.Replace(newdelimiter, ",");
+
+            }
+
+
+            input = input.Replace("\n", ",");
+           
+            if (IsEmptyString(input))
+            {
+                return 0;
+            }
+            String[] numbersArray = input.Split(new[] {delimiter },StringSplitOptions.RemoveEmptyEntries);
+            var sum = SumNumbers(numbersArray);
+            return sum;
+        }
+        private static String removeDelimeters(String input)
+        {
+            char[] charArray = input.ToCharArray();
+            char back = ' ';
+            String newStr = "";
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                back = charArray[i];
+                if (back == '\n')
+                {
+                    newStr = newStr + ",";
+                }
+                else
+                {
+                    newStr = newStr + charArray[i];
+                }
+            }
+            return newStr;
+        }
+
+        private static int SumNumbers(string[] numbersArray)
+        {
+            int sum = 0;
+            int[] intArray; 
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                int current = int.Parse(numbersArray[i]);
+                if (current > 0)
+                {
+                    sum = sum + current;
+                }
+                else
+                {
+                    //throw new NegativesNotAllowedException();
+                }
+            }
+
+            return sum;
+        }
+
+        private static bool IsEmptyString(string input)
+        {
+            return input == "";
+        }
     }
 }
